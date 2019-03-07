a5b74b3202855e6cdcd56b9cdca46abfc47f2a5e
Brad T <bradthurlow@gmail.com>
2019-03-07 11:42:18 +1100

    6B and 7

---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Brad Thurlow on 6/3/19.
+//  Copyright © 2019 Brad Thurlow. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
